ec837fa7bcd86b1d8e6a8dbb9afc292e82e16754
DarkAnt <darkant@gmail.com>
2016-01-23 23:29:06 -0500

    Fixed jekyll.sh script and added some documentation

--- jekyll.sh
+++ jekyll.sh
@@ -4,10 +4,35 @@ source ./config.sh
 source utils/install.sh
 
 # [web server]
-source nginx.sh
+source ./nginx.sh
 
 # [jekyll deps]
-pkg_install ruby rubygems
+pkg_install gcc ruby ruby-devel rubygems
 
 # [jekyll]
 sudo gem install jekyll
+
+# [example use]
+# mkdir ~/blog
+# create files and dirs as shown here: http://jekyllrb.com/docs/structure/
+# .
+# ├── _config.yml
+# ├── _drafts
+# |   ├── begin-with-the-crazy-ideas.textile
+# |   └── on-simplicity-in-technology.markdown
+# ├── _includes
+# |   ├── footer.html
+# |   └── header.html
+# ├── _layouts
+# |   ├── default.html
+# |   └── post.html
+# ├── _posts
+# |   ├── 2007-10-29-why-every-programmer-should-play-nethack.textile
+# |   └── 2009-04-26-barcamp-boston-4-roundup.textile
+# ├── _data
+#     |   └── members.yml
+#     ├── _site
+#     ├── .jekyll-metadata
+#     └── index.html
+
+# jekyll build --source ~/blog --destination /var/www/html
--- utils/install.sh
+++ utils/install.sh
@@ -1,4 +1,4 @@
-source ../config.sh
+source ./config.sh
 
 function pkg_install() {
     sudo $PKG_MGR install $@ -y
